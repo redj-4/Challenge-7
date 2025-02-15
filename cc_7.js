523a7775612805d801f5be4092d40715ac7ff043
Divya Joshi <divyaj.redj@gmail.com>
2025-02-15 10:58:16 -0500

    Task 2 – Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -7,3 +7,13 @@ function calculateInvoice(subtotal, taxRate, discount) {
   // Test Data
   console.log(calculateInvoice(100, 0.08, 5));  // Expected output: "Total Invoice: $103.00"
   console.log(calculateInvoice(500, 0.1, 20));  // Expected output: "Total Invoice: $530.00"
+
+  //Task 2: Function Expression
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`;
+  };
+  
+  // Test Data
+  console.log(calculateHourlyWage(52000, 40));  // Expected output: "Hourly Wage: $25.00"
+  console.log(calculateHourlyWage(75000, 35));  // Expected output: "Hourly Wage: $41.21"
